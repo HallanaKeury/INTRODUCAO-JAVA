35c487607a4a6c53f8e43a85dada6938dc4c1992
HallanaKeury <hallanasousa10@gmail.com>
2022-07-29 20:06:27 -0300

    Primeiro exercício de lógica condicional

---
+++ LogicaCondicionalEControleDeFluxos/src/OperadoresRelacionais/PrimeiroExercicio.java
@@ -0,0 +1,46 @@
+package OperadoresRelacionais;
+
+//Exercício para comparar operadores relacionais
+
+public class PrimeiroExercicio {
+
+    public static void main(String[] args) {
+
+        int i1 = 10;
+        int i2 = 20;
+        float f1 = 4.5f;
+        float f2 = 3.5f;
+        double d1 = 7.8;
+        double d2 = 5.7;
+        char c1 = 'x';
+        char c2 = 'y';
+        String s1 = "Fulano";
+        String s2 = "Fulano";
+        boolean b1 = true;
+        boolean b2 = false;
+
+        long l1 = 54676L;
+        long l2 = 65428L;
+        byte y1 = 1;
+        byte y2 = 25;
+
+        //Comparações
+        System.out.println("i1 == i2 " + (i1 == i2));
+        System.out.println("i1 != i2 " + (i1 != i2));
+        System.out.println("i1 >= i2 " + (i1 >= i2));
+        System.out.println("i1 > i2 " +  (i1 > i2));
+
+        /*
+        *  - Simbologia:
+            > Igualdade: ==
+            > Diferença: !=
+            > Maior: >
+            > Maior ou igual: >=
+            > Menor: <
+            > Menor igual: >=
+        *
+        * */
+
+
+    }
+}
